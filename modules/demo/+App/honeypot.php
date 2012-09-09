571064043bf7dcaf25102f2b645051fad9ad027f
srcspider <source.spider@gmail.com>
2012-09-09 23:13:57 +0300

    including missing files

--- modules/demo/+App/honeypot.php
+++ modules/demo/+App/honeypot.php
@@ -7,9 +7,8 @@
 
 class Context_About extends \demo\core\Context_About { /** @return \demo\core\Context_About */ static function instance() { return parent::instance(); } }
 class Context_Demo extends \demo\core\Context_Demo { /** @return \demo\core\Context_Demo */ static function instance() { return parent::instance(); } }
-class Context_Thor extends \demo\core\Context_Thor { /** @return \demo\core\Context_Thor */ static function instance() { return parent::instance(); } }
 class Controller_About extends \demo\core\Controller_About { /** @return \demo\core\Controller_About */ static function instance() { return parent::instance(); } }
 class Controller_Demo extends \demo\core\Controller_Demo { /** @return \demo\core\Controller_Demo */ static function instance() { return parent::instance(); } }
-class Controller_Thor extends \demo\core\Controller_Thor { /** @return \demo\core\Controller_Thor */ static function instance() { return parent::instance(); } }
+class Schematic_Demo_Example_Base extends \demo\core\Schematic_Demo_Example_Base { /** @return \demo\core\Schematic_Demo_Example_Base */ static function instance() { return parent::instance(); } }
 trait Trait_Context_DemoBase { use \demo\core\Trait_Context_DemoBase; }
 trait Trait_Controller_DemoBase { use \demo\core\Trait_Controller_DemoBase; }
