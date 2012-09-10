380e500ac02ffb57f681003f7d918e0e5d4f4898
srcspider <source.spider@gmail.com>
2012-09-10 19:56:58 +0300

    wording++

--- themes/classic/demofiles/about.php
+++ themes/classic/demofiles/about.php
@@ -11,40 +11,47 @@
 
 	<<?= $h ?>>Mjölnir</<?= $h ?>>
 
-	<p>The Mjölnir framework is designed for working within the context of a team, not as an
-	individual. But you can work just fine alone if you wish. Currently Mjölnir is in alpha going
-	by our <a href="https://github.com/ibidem/ibidem/blob/master/versioning.md">strict no-nonsense versioning rules</a>.
-	Alpha here doesn't actually refer to the quality but how nobody has bothered releasing v1.0
-	(ie. documentation for various features is not present in full, etc). By common versioning this
-	would be known as a "perpetual beta" state. Still, some caution is advised when using Mjölnir
-	in it's current state with out some knowledge of it's internals.</p>
+	<p>Mjolnir is a flexible and unbiased, PHP framework focused on getting things done. It is designed
+	to interoperate with virtually every piece of PHP code you can find, as well as be as simple,
+	straight forward, and customizable as possible; practically everything within it can be overwritten
+	to meet your needs.</p>
 
-	<p>Mjölnir is a completely unbiased framwork. <i>It is whatever you want it to be.</i> There is
-	no core to the framework, it's just a series of modules you load that make it into what it is.
-	If you don't like something you just don't load it or use something else. By default for example
-	in the case of serving web pages the framwork applies the following stack: Access, HTTP, HTML,
-	MVC. But maybe that doesn't make sense to you. With one configuration change you can just have
-	it do Logger, ACL, HTTP, Application, DCI (just as an example) instead. Maybe it's not a page
-	request at all, maybe it's a console application: Console (it's usually just that). Whatever
-	you want, as long as it's within the scope of PHP as a language, you can make Mjölnir handle it
-	for you in an organized and efficient way.</p>
+	<p>Currently Mjölnir is in alpha, going by our
+	<a href="https://github.com/ibidem/ibidem/blob/master/versioning.md">strict no-nonsense
+	versioning rules</a>. Alpha here doesn't actually refer to the quality but how nobody has
+	bothered releasing v1.0 (see: versioning for info). By common versioning conventions this
+	would be known as a "perpetual beta" state. That being said, some caution is advised when
+	using Mjölnir in it's current state with out some knowledge of it's internals.</p>
 
+	<p>Mjölnir is <i>whatever you want it to be.</i> There is no core to the framework, it's just
+	a series of modules you load that make it into what it is. If you don't like something you just
+	don't load it or use something else. Don't like the structure, you can just make your own!
+	In the case of serving web pages, for example, the framework applies the following stack:
+	Access, HTTP, HTML, MVC. But maybe that doesn't make sense to you. With one configuration change
+	you can have it do: Logger, ACL, HTTP, Application, DCI instead. Of course it can potentially
+	not be a page request at all, Mjölnir doesn't care. Whatever you want, as long as it's within
+	the scope of PHP as a language, you can make Mjölnir handle it for you in an organized way.</p>
 
 	<<?= $h ?>>Themes in Mjölnir</<?= $h ?>>
 
+	<p>Mjölnir is designed to handle web apps around themes.</p>
+
 	<p>
-		Mjölnir is designed to handle web apps around themes. In other frameworks you may find views
+		In other frameworks you may find views
 		(the files within themes) to be very dumb files, usually for the sake of unit testing,
-		but in Mjölnir by default they have a lot of power and control, the goal being that once one
-		theme is made any number of themes can be made with no (or very minimal) intervention required
-		from the programmer end. Designers thus have full control over the files, and structure within
-		the themes; with the exception of the configuration file <code>+theme.php</code> every single
-		file and folder, along with which files make what page, is decided (entirely) by the designer
-		and not some programming contract. Themes do not get raw data, they instead get hooks via a
-		main context and are able to ask for what they need. A theme can have an element, while
+		but in Mjölnir by default <small class="muted">(you may change it if you wish)</small> views
+		have a lot of power and control. The goal is to allow any number of themes to be made with no
+		(or very minimal) intervention required from the programmers end, allowing designers to have full
+		control over the files, and structure within the themes — with the exception of the configuration
+		file <code>+theme.php</code> every single file and folder, along with which files make what page,
+		is decided (entirely) by the designer and not some programming contract. </p>
+
+	<p>By default themes also do not get raw data. They instead get hooks via a
+		main context and are able to ask for what they need. One theme can have an element, while
 		another might not, in which case any processing required for getting that element will be
-		completely void as well. A theme can also spawn additional contexts from the main context to
-		insert elements on pages that were previously only for other pages.
+		void as well. A theme can also spawn additional contexts from the main context it is given
+		allowing it to insert elements that might be originally intended for other pages; this of
+		course depends on how powerful it's main context is.
 	</p>
 
 </div>
--- themes/classic/demofiles/demo.php
+++ themes/classic/demofiles/demo.php
@@ -32,12 +32,12 @@
 	</p>
 
 	<p>
-		To see the style you need to activate the monitors. So in the case of the demo you just go to
+		To see the style you need to activate the watchers. So in the case of the demo you just go to
 		the theme directory <code><?= DOCROOT.'themes'.$ds.'classic' ?></code> and launch the style
 		monitor <code><?= '+styles'.$ds.'default'.$ds.'+start.rb' ?></code> and script monitor
 		<code><?= '+scripts'.$ds.'+start.rb' ?></code> which look at your files for changes and
-		compile as necessary as they happen. <small class="muted">(by default scripts run google's
-		closure compiler and styles run the sass compiler via compass)</small>
+		compile as necessary. <small class="muted">(by default scripts run google's closure compiler
+		and styles run the sass compiler via compass)</small>
 	</p>
 
 	<hr/>
@@ -51,5 +51,41 @@
 	</ol>
 
 	<p>All done!</p>
+
+	<hr/>
+
+	<p><big>Moving forward...</big></p>
+
+	<p>There a few approaches to using the template.</p>
+
+	<ol>
+		<li><p>The most obvious way is to just remove the <code>.git</code>
+			directory (<code>rm -rf .git</code>), re-initialize the directory
+			(<code>git init</code>) and then use it as the initial structure
+			(<code>git add .</code>, <code>git commit -m 'initial structure'</code>).</p>
+		</li>
+
+		<li><p>Another way would be to just branch out and do your work in the
+			new branch. May seem silly, but if you just want to use the template
+			for quick testing, prototyping or some other isolated development
+			technique, it works great since you can easily pull in template updates, as
+			well as maintain multiple such prototypes in the same project. You can go
+			further and rename the remote (<code>git remote rename origin mjolnir</code>)
+			and your own <code>origin</code> and then use it to push a clean master
+			to your own remote while keeping the template remote for updates.</p>
+		</li>
+
+		<li><p>Yet another way is to use the method above to create your own template,
+			if you find yourself repeating creating repetative project structure over
+			and over. You then re-apply the methodology but for your own custom template.
+			The idea here is you might want a completely 
+			different project structure then the current default.
+			<small class="muted">(yes it's all customizable and in your hands)</small> Or
+			you might just have your own favorite tools (Doctrine? Vagrant?). Or maybe you
+			just have some default js/scss plugins/libraries you want to be there. Making
+			your own template, or a specialized template, can help a lot.</p>
+		</li>
+
+	</ol>
 
 </div>
