ddcee4ff153a022fa5cc9663de12dbb5a45af2d1
srcspider <source.spider@gmail.com>
2012-09-12 00:28:23 +0300

    updating honeypot

--- modules/demo/+App/honeypot.php
+++ modules/demo/+App/honeypot.php
@@ -7,10 +7,10 @@
 
 class Context_About extends \demo\core\Context_About { /** @return \demo\core\Context_About */ static function instance() { return parent::instance(); } }
 class Context_Demo extends \demo\core\Context_Demo { /** @return \demo\core\Context_Demo */ static function instance() { return parent::instance(); } }
-class Context_Quickstart extends \demo\core\Context_Quickstart { /** @return \demo\core\Context_Quickstart */ static function instance() { return parent::instance(); } }
+class Context_Start extends \demo\core\Context_Start { /** @return \demo\core\Context_Start */ static function instance() { return parent::instance(); } }
 class Controller_About extends \demo\core\Controller_About { /** @return \demo\core\Controller_About */ static function instance() { return parent::instance(); } }
 class Controller_Demo extends \demo\core\Controller_Demo { /** @return \demo\core\Controller_Demo */ static function instance() { return parent::instance(); } }
-class Controller_Quickstart extends \demo\core\Controller_Quickstart { /** @return \demo\core\Controller_Quickstart */ static function instance() { return parent::instance(); } }
+class Controller_Start extends \demo\core\Controller_Start { /** @return \demo\core\Controller_Start */ static function instance() { return parent::instance(); } }
 class Schematic_Demo_Example_Base extends \demo\core\Schematic_Demo_Example_Base { /** @return \demo\core\Schematic_Demo_Example_Base */ static function instance() { return parent::instance(); } }
 trait Trait_Context_DemoBase { use \demo\core\Trait_Context_DemoBase; }
 trait Trait_Controller_DemoBase { use \demo\core\Trait_Controller_DemoBase; }
