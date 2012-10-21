a2767cb2d68c4ffd079de1acfe567cf84e37a3ab
srcspider <source.spider@gmail.com>
2012-10-21 14:29:20 +0300

    updated mjolnir.php configuration

--- mjolnir.php
+++ mjolnir.php
@@ -1,19 +1,22 @@
 <?php namespace app;
 
+#
+# The following has been condensed to be as short as possible and updatable via
+# the system modules. You may customize it to suit your own needs. If you don't
+# want to customize your projects structure, you can just forget this file even
+# exists.
+#
+
+// gurantee EXT is defined
 \defined('EXT') or \define('EXT', '.php');
 
-# ---- Basic settings -------------------------------------------------------- #
-
-// define handy shorthand :)
-$ds = DIRECTORY_SEPARATOR;
-
 // set the full path to the docroot
-\define('DOCROOT', \realpath(\dirname(__FILE__)).$ds);
+\define('DOCROOT', \realpath(\dirname(__FILE__)).'/');
 
-$cfspath = DOCROOT.'vendor'.$ds.'mjolnir'.$ds.'cfs'.$ds.'+App'.$ds;
+$cfspath_files = DOCROOT.'vendor/mjolnir/cfs/+App/';
 
 // fail gracefully when libraries are missing
-if ( ! \file_exists($cfspath))
+if ( ! \file_exists($cfspath_files))
 {
 	if (\defined('PUBDIR'))
 	{
@@ -37,90 +40,11 @@ if ( ! \file_exists($cfspath))
 
 // we load the default environment; if you want custom APPPATH, MODPATH, PLGPATH
 // simply define them before this statement, they will not be redefined.
-require $cfspath.'default.mjolnir.php';
+require $cfspath_files.'default.mjolnir.php';
 
+#
+# See [mjolnir/cfs/+App/bridges] for bridge declaration help
+#
 
-# ---- Modules --------------------------------------------------------------- #
-
-if (\defined('PUBDIR'))
-{
-	$pubdir_config = include PUBDIR.'config'.EXT;
-}
-
-$env_config = include 'environment'.EXT;
-
-// setup the modules
-CFS::modules($env_config['modules']);
-
-// allow application to store and overwrite config files, routes, etc;
-// everything except classes. You should always define your classes in
-// appropriate modules in the MODPATH
-CFS::add_frontpaths([APPPATH]);
-
-// attempt to load private configuration
-if (\defined('PUBDIR'))
-{
-	$pubdir_config = include PUBDIR.'config'.EXT;
-	if (isset($pubdir_config['private.files']) && \file_exists($pubdir_config['private.files']))
-	{
-		\app\CFS::add_frontpaths([$pubdir_config['private.files']]);
-	}
-}
-else # console or other
-{
-	$base_config = include APPPATH.'config/mjolnir/base'.EXT;
-	if (\file_exists($base_config['private.files']))
-	{
-		\app\CFS::add_frontpaths([$base_config['private.files']]);
-	}
-}
-
-// you are not suppose to overwrite namespaces and abstracts; that's a misuse.
-// hence it makes no sense to search for them in \app\Class calls. Namespaces
-// should always be explicit
-CFS::add_namespaces($env_config['namespaces']);
-
-$base_config = \app\CFS::config('mjolnir/base');
-
-// see: http://php.net/timezones
-\date_default_timezone_set($base_config['timezone']);
-
-// see: http://php.net/setlocale
-\setlocale(LC_ALL, $base_config['locale.lang'].$base_config['charset']);
-
-# ---- Bridges --------------------------------------------------------------- #
-/*
-\define('SYSPATH', true); # kohana3 file-safeguard requirement
-
-// setup the bridges
-\mjolnir\cfs\Kohana3_Bridge::bridges
-	(
-		array
-		(
-			PLGPATH.'kohana3'.$ds.'core' => array
-				(
-					'namespace' => 'kohana3\core',
-					'prefix' => 'kohana',
-				),
-		)
-	);
-
-// include the paths in the CFS (note: this is not the same as adding modules)
-CFS::add_backpaths(\mjolnir\cfs\Kohana3_Bridge::paths());
-
-// register the autoloader
-\spl_autoload_register( array('\mjolnir\cfs\Kohana3_Bridge', 'load_class') );
-*/
-
-
-# ---- Composer Autoloaders -------------------------------------------------- #
-
-if (\file_exists(PLGPATH.$ds.'autoload'.EXT))
-{
-	// composer setup
-	require PLGPATH.$ds.'autoload'.EXT;
-}
-
-
-// done; cleaning up
-unset($ds, $cfspath);
+// cleanup
+unset($cfspath_files);
