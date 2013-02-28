d221300f920fa4718e8c19f26cecc187bf4b02d0
srcspider <source.spider@gmail.com>
2013-02-28 09:37:23 +0200

    APPPATH is now etc

--- modules/demo/+App/config/routes.php
+++ modules/demo/+App/config/routes.php
@@ -10,4 +10,9 @@
 		'/login(/<action>)'
 			=> [ 'login.public', [ 'action' => '(signin|signout|pwdreset)' ] ],
 
+	// aliases to static content
+	
+		'/thumbs/timthumb-2.8.10.php?src=<image>&w=<width>&h=<height>'
+			=> [ 'image.static' ], # parameters not required
+	
 	);
