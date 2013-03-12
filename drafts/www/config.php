cec8e3c317d069ab6ffb499e2d2d0a03fe5b4886
srcspider <source.spider@gmail.com>
2013-03-12 18:19:54 +0200

    updated drafts file with logging information

--- drafts/www/config.php
+++ drafts/www/config.php
@@ -30,6 +30,43 @@ return array
 		// turns on/off caching globally
 		'caching' => true,
 
+		// logging system settings
+		'logging' => array
+			(
+				// turning duplication will cause the logging system to relog
+				// re-occuring errors based on their main exception message hash
+				// with the option off only the first occurance will be recorded
+				// [!!] does not apply to the short.log
+				'duplication' => false,
+
+				// the logging system will replicate all errors based on their
+				// level key. So "Notice" errors will get replicated into Notice
+				// Hacking will get replicated into Hacking. This can be very
+				// efficient way of managing your log. For integrity reasons the
+				// master log will still hold the errors regardless
+				'replication' => false,
+
+				// the short.log or devlog stores a 1-line version of the error
+				// this is very useful in development where most errors can be
+				// easily identified in a few words and don't need to be stored
+				'short.log' => true,
+
+				// you may ignore certain types of log errors if you already
+				// an alternative system in place that catches them and reports
+				// them to you; one such case are 404 errors
+				'exclude' => [ '404' ],
+
+				// sometimes you may be recieving errors from underlying or
+				// proxy systems outside your control. Often these are caused
+				// by broken client side javascript, that makes its way to the
+				// the server, insert any regular expression bellow if the main
+				// message matches the pattern it will be ignored
+				'filter' => array
+					(
+						// no regex patterns
+					),
+			),
+
 		// turn on maintainence mode when performing long running modifications
 		// in a live environment; you can customize the placeholder downtime.php
 		// page for customized maintainence message and view
@@ -41,7 +78,7 @@ return array
 				'retry-after' => null, # format: Sat, 8 Oct 2011 18:27:00 GMT
 
 				// allows you to bypass the block
-				'passcode' => 'thereisnocowlevel',
+				'passcode' => 'opensesame',
 			),
 
 		// additional information required by various processes
